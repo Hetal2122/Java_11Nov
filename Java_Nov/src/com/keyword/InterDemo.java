cacfb59e138c4be9ad880f10df0049ffaff48eae
Hetal2122 <naykahetu.2122@gmail.com>
2024-12-21 14:19:38 +0530

    17 dec

---
+++ Java_Nov/src/com/keyword/InterDemo.java
@@ -0,0 +1,22 @@
+package com.keyword;
+
+/* 17  dec 2024
+ * class--->interface   implements
+ * class--->class       extends
+ * interface->interface extends
+ */
+
+
+
+interface Inter1{
+	int i1=202;
+	
+}
+
+public interface InterDemo extends Inter1 {
+
+	int i_no=102;
+	
+	public void show();
+
+}
---
+++ Java_Nov/src/com/keyword/StaticDemo.java
@@ -0,0 +1,32 @@
+package com.keyword;
+
+/*
+ * 17 dec 24 
+ * 
+ * Static : constant at memory side
+ * 
+ * use with variable: opposite of final
+ * use with method: 
+ * 1.only static members are allowed in static method
+ * 2.all members inside static method are static by default
+ * 3.before static method was called by class only
+ * but now we can call static by object.
+ */
+
+
+ class SDemo{
+	int no;
+	static int s_no;
+	
+//	public static void show() {
+//		s_no=123;
+//		int r_no=678;
+//		System.out.println("s no is :"+s_no);
+//		System.out.println("r no is :"+r_no);
+//		
+//	}
+}v
+
+public class StaticDemo {
+
+}
